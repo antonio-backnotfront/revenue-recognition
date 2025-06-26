36d3002863b88015dbee925795fd52014515bd2d
Anton Solianyk <solyanicks@gmail.com>
2025-06-26 14:03:43 +0200

    fix: change nullabulity of IsLoyal

--- src/RevenueRecognition.Models/Client/Client.cs
+++ src/RevenueRecognition.Models/Client/Client.cs
@@ -12,7 +12,7 @@ public class Client
     [MaxLength(255)] public string? Address { get; set; }
     [MaxLength(255)] public string? Email { get; set; }
     [MaxLength(255)] public string? PhoneNumber { get; set; }
-    public bool? IsLoyal { get; set; }
+    public bool IsLoyal { get; set; }
     public ICollection<Subscription> Subscriptions { get; set; }
     public ICollection<Contract> Contracts { get; set; }
     public IndividualClient? IndividualClient { get; set; }
