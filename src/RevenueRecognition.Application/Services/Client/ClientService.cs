6b32d465f3ddb0a6ae6d89e4c516f587eeef4c5f
Anton Solianyk <solyanicks@gmail.com>
2025-06-26 03:55:06 +0200

    feat(Services): add raw methods to IClientService and create its implementation class

---
+++ src/RevenueRecognition.Application/Services/Client/ClientService.cs
@@ -0,0 +1,31 @@
+using RevenueRecognition.Application.DTOs.Client;
+
+namespace RevenueRecognition.Application.Services.Client;
+
+public class ClientService : IClientService
+{
+    public Task<List<GetClientResponse>> GetClientsAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CreateClientRequest> CreateClientAsync(CreateClientRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> RemoveClientAsync(int id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GetClientResponse> UpdateClientAsync(int id, UpdateClientDto dto, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GetClientResponse> GetClientByIdAsync(int id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/RevenueRecognition.Application/Services/Client/IClientService.cs
+++ src/RevenueRecognition.Application/Services/Client/IClientService.cs
@@ -1,6 +1,12 @@
+using RevenueRecognition.Application.DTOs.Client;
+
 namespace RevenueRecognition.Application.Services.Client;
 
 public interface IClientService
 {
-    
+    public Task<List<GetClientResponse>> GetClientsAsync(CancellationToken cancellationToken);
+    public Task<CreateClientRequest> CreateClientAsync(CreateClientRequest request, CancellationToken cancellationToken);
+    public Task<bool> RemoveClientAsync(int id, CancellationToken cancellationToken);
+    public Task<GetClientResponse> UpdateClientAsync(int id, UpdateClientDto dto, CancellationToken cancellationToken);
+    public Task<GetClientResponse> GetClientByIdAsync(int id, CancellationToken cancellationToken);
 }
