1daf99c4c517f43aeaf8b1828b6f1a15da6cc12c
Anton Solianyk <solyanicks@gmail.com>
2025-06-26 05:39:52 +0200

    feat(Models&DTO): modify nullability of fields

--- src/RevenueRecognition.Application/DTOs/Client/GetClientResponse.cs
+++ src/RevenueRecognition.Application/DTOs/Client/GetClientResponse.cs
@@ -3,9 +3,9 @@ namespace RevenueRecognition.Application.DTOs.Client;
 public class GetClientResponse
 {
     public int Id { get; set; }
-    public string Address { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
+    public string? Address { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
     public bool IsLoyal { get; set; }
     public GetCompanyClientDto? CompanyInformation { get; set; }
     public GetIndividualClientDto? IndividualInformation { get; set; }
